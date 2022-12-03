398ecebce2ed3225f408228430b4fb2218d03745
Marcusksi <60144914+Marcusksi@users.noreply.github.com>
2022-12-03 21:48:20 +0200

    uued asjad

---
+++ kontakt_js.js
@@ -0,0 +1,10 @@
+
+const form = document.getElementsById('vorm'); //teeb muutuja, mis on kontaktvorm
+
+//
+form.addEventListener('submit', function onSubmit(event) {
+  // tühjendab kontaktvormi väljad
+  form.reset();
+});
+
+
