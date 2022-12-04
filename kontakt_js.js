c38b06de1bfd8228a7149504948d77f23dff0906
Marcusksi <60144914+Marcusksi@users.noreply.github.com>
2022-12-04 19:22:28 +0200

    muudatused

--- kontakt_js.js
+++ kontakt_js.js
@@ -1,10 +1,10 @@
+//võetud siit: https://bobbyhadz.com/blog/javascript-clear-input-field-after-submit
+//autor on Borislav Hadzhiev
 
 const form = document.getElementsById('vorm'); //teeb muutuja, mis on kontaktvorm
 
-//
-form.addEventListener('submit', function onSubmit(event) {
-  // tühjendab kontaktvormi väljad
-  form.reset();
+form.addEventListener('submit', function onSubmit(event) { //vaatab, kas nupule vajutatakse
+  form.reset(); // tühjendab kontaktvormi väljad
 });
 
 
